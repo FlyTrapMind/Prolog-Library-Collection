79d63ae30bcb8987a775c8138212c51cf58cd2ef
Wouter Beek <me@wouterbeek.com>
2013-08-30 10:20:52 +0200

    Some work on asserting a DCG grammar based on an ABNF input file. Awaiting answer from the SWI-Prolog mailing list.
    * [DB_EXT,NEW] Added db_add_clause/[2,3].

--- DCG/abnf2dcg.pl
+++ DCG/abnf2dcg.pl
@@ -15,8 +15,18 @@ Converts ABNF grammars to DCGs.
 @version 2013/08
 */
 
+:- use_module(dcg(dcg_ascii)).
+:- use_module(dcg(dcg_cardinal)).
 :- use_module(dcg(dcg_content)).
+:- use_module(dcg(dcg_multi)).
 :- use_module(library(pio)).
+:- use_module(math(radix)).
+
+:- initialization(test).
+
+test:-
+  absolute_file_name(project('abnf.abnf'), File, [access(read)]),
+  abnf2dcg(File).
 
 
 
@@ -24,12 +34,46 @@ abnf2dcg(File):-
   access_file(File, read),
   phrase_from_file(abnf, File).
 
-abnf_rule -->
-  abnf_name, blanks, "=", blanks, abnf_elements, crlf.
 
-abnf_name -->
-  dcg_cistring.
 
-%  BIT            =  "0" / "1"
+% GRAMMAR %
 
+abnf -->
+  dcg_multi(rule).
+
+%! base(?Radix:oneof([2,10,16]))//
+
+base(2)  --> b.
+base(10) --> d.
+base(16) --> x.
+
+elements(Name) -->
+  element(Name).
+
+element(Name) -->
+  single_terminal_value(Code),
+  {Head =.. [Name,T1,T2]},
+  {Body =.. [
+  {db_add_clause(Head, Body)}.
+
+name(Name) -->
+  dcg_cistring(Name).
+
+rule -->
+  name(Name),
+  blanks, equals_sign, blanks,
+  elements(Name),
+  crlf.
+
+%! single_terminal_value(-Code:positive_integer)//
+% Reads a character code from the input stream.
+% The character code is described in a given radix or base
+% (either `2`, `10`, or `16`).
+
+single_terminal_value(Code) -->
+  percent_sign,
+  base(Radix),
+  hexadecimal_digit(H1),
+  hexadecimal_digit(H2),
+  {digits_to_decimal([H1,H2], Radix, Code)}.
 
