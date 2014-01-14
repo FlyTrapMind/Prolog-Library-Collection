5004f71bdcf9ea68d0e01af6ba7d0ace726e5337
Wouter Beek <me@wouterbeek.com>
2014-01-14 20:15:59 +0100

    Added Web presentation for RDF resources that occur in the datatype (typed literal) and predicate position (very useful for debugging!).

--- Standards/RDF/Web/rdf_tabular.pl
+++ Standards/RDF/Web/rdf_tabular.pl
@@ -20,6 +20,7 @@ Generated RDF HTML tables.
 :- use_module(library(lists)).
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdfs)).
+:- use_module(rdf(rdf_datatype)).
 :- use_module(rdf(rdf_dcg)).
 :- use_module(rdf(rdf_name)).
 :- use_module(rdf_web(rdf_html)).
@@ -35,8 +36,11 @@ Generated RDF HTML tables.
 rdf_tabular(Request):-
   memberchk(search(Search), Request),
   memberchk(term=Term, Search), !,
-  format(atom(Caption), 'Description of resource denoted by ~w.', [Term]),
-  reply_html_page(app_style, title(Caption), \rdf_tabular(Caption, Term)).
+  reply_html_page(
+    app_style,
+    title(['Overview of resource ',Term]),
+    \rdf_tabular(Term)
+  ).
 rdf_tabular(_):-
   reply_html_page(app_style, title('Overview of resources.'), \overview).
 
@@ -74,11 +78,65 @@ overview([H1|T]) -->
   overview(T).
 
 
-rdf_tabular(Caption, Term) -->
+rdf_tabular(Term) -->
   {
     dcg_phrase(rdf_term(S1), Term),
-    rdf_global_id(S1, S2),
-    setoff([P,O,G], rdf(S2, P, O, G), L)
+    rdf_global_id(S1, S2)
+  },
+  rdf_tabular_pl(S2).
+
+% Datatype (in typed literal).
+rdf_tabular_pl(D) -->
+  {
+    rdf_datatype(_, D), !,gtrace,
+    setoff([Value], rdf_datatype(_, _, D, Value, _), Values),
+    format(atom(Caption), 'Ordered value list for datatype ~w.', [D])
+  },
+  html_table(
+    [caption(Caption),cell_dcg(rdf_html_term),header(true),indexed(true)],
+    [['Value']|Values]
+  ).
+% Predicate term.
+rdf_tabular_pl(P) -->
+  {
+    O0 = [cell_dcg(rdf_html_term),header(true),indexed(true)],
+    rdf(_, P, _), !,
+    format(atom(Caption1), 'Domain of property ~w.', [P]),
+    merge_options([caption(Caption1)], O0, O1),
+    setoff([C], (rdf(S, P, _), rdfs_individual_of(S, C)), Cs1),
+    format(atom(Caption2), 'Range of property ~w.', [P]),
+    merge_options([caption(Caption2)], O0, O2),
+    setoff([C], (rdf(_, P, O), rdfs_individual_of(O, C)), Cs2)
+  },
+  html_table(O1, [['Class']|Cs1]),
+  html_table(O2, [['Class']|Cs2]),
+  
+  % For literal ranges we also display the values that occur.
+  {
+    setoff([Value], value_for_p(P, Value), Values),
+    format(atom(Caption3), 'Values that occur for property ~w.', [P]),
+    merge_options([caption(Caption3)], O0, O3),
+    length(Values, Length)
+  },
+  (
+    % Do not display tables that are crazy big.
+    {Length > 1000}, !
+  ;
+    html_table(O3, [['Value']|Values])
+  ).
+% Subject term.
+rdf_tabular_pl(S) -->
+  {
+    setoff([P,O,G], rdf(S, P, O, G), L),
+    format(atom(Caption), 'Description of resource denoted by ~w.', [S])
   },
   rdf_html_table(Caption, L).
+
+value_for_p(P, Value):-
+  rdf(_, P, literal(type(_,Value))).
+value_for_p(P, Value):-
+  rdf(_, P, literal(lang(_,Value))).
+value_for_p(P, Value):-
+  rdf(_, P, literal(Value)),
+  \+ compound(Value).
 
