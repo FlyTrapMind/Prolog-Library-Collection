7338453f2bf71be669e74f83c15c19de044f99bc
Wouter Beek <me@wouterbeek.com>
2014-01-15 14:04:42 +0100

    NEW: Calling goals on HTTP-accessible content with http_goal/[3,4]. Automatically retries after exceptions and non-success status codes.

--- Standards/HTTP/http.pl
+++ Standards/HTTP/http.pl
@@ -2,9 +2,13 @@
   http,
   [
     http_dateTime/1, % -DateTime:term
-    http_open_wrapper/3, % +URI:uri
-                         % -Stream:stream
-                         % +Options:list(nvpair)
+    http_goal/3, % +URL:atom
+                 % +Options:list(nvpair)
+                 % :Goal,
+    http_goal/4, % +URL:atom
+                 % +Options:list(nvpair)
+                 % :Goal,
+                 % +Attempts:or([integer,oneof([inf])])
     serve_nothing/1, % +Request:list
     xml_serve_atom/1, % +XML:atom
     xml_serve_dom/2 % +Options:list(nvpair)
@@ -17,15 +21,21 @@
 Predicates for sending out HTTP requests.
 
 @author Wouter Beek
-@version 2012/10, 2013/02, 2013/11
+@version 2012/10, 2013/02, 2013/11, 2014/01
 */
 
+:- use_module(math(math_ext)).
 :- use_module(library(debug)).
 :- use_module(library(http/http_header)).
 :- use_module(library(http/http_open)).
+:- use_module(library(option)).
 :- use_module(xml(xml_dom)).
 
-:- meta_predicate(http_process_exception(+,0)).
+:- meta_predicate(http_goal(+,+,1)).
+:- meta_predicate(http_goal(+,+,1,+)).
+:- meta_predicate(http_open_catcher(+,+,+,1,+)).
+:- meta_predicate(http_open_exception(+,0)).
+:- meta_predicate(http_open_process(+,+,1)).
 
 
 
@@ -38,61 +48,92 @@ http_dateTime(DateTime):-
   get_time(TimeStamp),
   http_timestamp(TimeStamp, DateTime).
 
-%! http_open_wrapper(+URI:uri, -Stream:stream, +Options:list(nvpair)) is det.
-% Retries the given URI a specific number of times (currently `5`).
-% This helps when working with unreliable connections.
 
-http_open_wrapper(URI, Stream, Options):-
-  http_open_wrapper(URI, Stream, Options, 0).
+%! http_goal(+URL:atom, +Options:list(nvpair), :Goal) is semidet.
+%! http_goal(
+%!   +URL:atom,
+%!   +Options:list(nvpair),
+%!   :Goal,
+%!   +Attempts:or([integer,oneof([inf])])
+%! ) is semidet.
+% Executes the given goal on the content found at the given URL.
+%
+% The number of attempts is the number of HTTP-related exceptions
+%  that can be encountered before the predicate gives up.
+%
+% The arguments of `Goal` are appended with the argument `Stream`.
 
-% The maximum number of HTTP attempts has been made.
-http_open_wrapper(URI, _Stream, _Options, 5):- !,
+http_goal(URL, Options, Goal):-
+  http_goal(URL, Options, Goal, 5).
+
+http_goal(URL, _, Goal, 0):- !,
   debug(
     http,
-    'The maximum number of HTTP attempts was reached for <~w>.',
-    [URI]
+    'Could not execute goal ~w on URL ~w (no attempts left).',
+    [Goal,URL]
   ).
-http_open_wrapper(URI, Stream, Options, Attempts):-
-  catch(
+http_goal(URL, O1, Goal, Attempts):-
+  merge_options([status_code(Status)], O1, O2),
+  setup_call_catcher_cleanup(
+    http_open(URL, Stream, O2),
+    http_open_process(Status, Stream, Goal),
+    Catcher,
     (
-      http_open(URI, Stream, Options),
-      set_stream(Stream, encoding(utf8))
-    ),
-    Exception,
-    (
-      NewAttempts is Attempts + 1,
-      http_process_exception(
-        Exception,
-       http_open_wrapper(URI, Stream, Options, NewAttempts)
-      )
+      close(Stream),
+      http_open_catcher(Catcher, URL, O2, Goal, Attempts)
     )
   ).
 
+http_open_catcher(exit, URL, _, Goal, _):- !,
+  debug(http, 'Successfully performed goal ~w on URL ~w.', [Goal,URL]).
+http_open_catcher(Catcher, URL, Options, Goal, Attempts1):-
+  gtrace,
+  debug(
+    http,
+    'Encountered exception ~w while executing goal ~w on URL ~w.',
+    [Catcher,Goal,URL]
+  ),
+  count_down(Attempts1, Attempts2),
+  http_open_exception(Catcher),
+  http_goal(URL, Options, Goal, Attempts2).
+
 % Retry after a while upon existence error.
 % Thrown by http_open/3
-http_process_exception(error(existence_error(url, URI), Context), Goal):- !,
-  debug(http, 'Resource <~w> does not seem to exist.', [URI]),
+http_open_exception(error(existence_error(url, URL),Context)):- !,
+  debug(http, 'URL ~w does not exist.', [URL]),
   debug(http, '[~w]', [Context]),
-  sleep(10),
-  call(Goal).
+  sleep(10).
 % Retry upon socket error.
 % Thrown by http_open/3.
-http_process_exception(error(socket_error('Try Again'), _Context), Goal):- !,
+http_open_exception(error(socket_error('Try Again'),_Context)):- !,
   debug(http, '[SOCKET ERROR] Try again!', []),
-  call(Goal).
+  sleep(1).
 % Retry upon I/O error.
-%http_process_exception(
-%  error(io_error(read, _Stream), context(_Predicate, Reason)),
-%  Goal
-%):- !,
-%  debug(http, '[IO-EXCEPTION] ~w', [Reason]),
-%  call(Goal).
-http_process_exception(Exception, _Goal):-
+http_open_exception(
+  error(io_error(read,_Stream),context(_Predicate,Reason))
+):- !,
+  debug(http, '[IO-EXCEPTION] ~w', [Reason]).
+http_open_exception(Exception):-
+  gtrace, %DEB
   debug(http, '!UNRECOGNIZED EXCEPTION! ~w', [Exception]).
+
+% Success codes.
+http_open_process(Status, Stream, Goal):-
+  between(200, 299, Status), !,
+  call(Goal, Stream).
+% Non-success codes.
+http_open_process(Status, _, _):-
+  gtrace,
+  debug(http, '[HTTP STATUS CODE] ~d', [Status]),
+  sleep(10),
+  % The catcher has to make a new attempt (if there are any attempts left).
+  throw(error(http_status(Status),_Context)).
+
 
 serve_nothing(Request):-
   memberchk(pool(client(_, _ , _In, Out)), Request),
   http_reply_header(Out, status(no_content), []).
+
 
 %! xml_serve_atom(+XML:atom) is det.
 % Serves the given XML-formatted atom.
@@ -103,6 +144,7 @@ xml_serve_atom(XML):-
   % Prolog throws an IO exception on `format(XML)`.
   format('Content-type: application/xml; charset=utf-8~n~n'),
   format(XML).
+
 
 %! xml_serve_dom(+Options:list(nvpair), +DOM:list) is det.
 % Serves the given XML DOM.
