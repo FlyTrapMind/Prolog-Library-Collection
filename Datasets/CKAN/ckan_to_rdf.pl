ff4ece8e78b3588ab0a6014c88eae60acd07116f
Wouter Beek <me@wouterbeek.com>
2014-01-19 15:13:29 +0100

    Uses two new things:   1. The new, integrated list script.   2. THe RDF-based and PL-based CKAN API.

--- Datasets/CKAN/ckan_to_rdf.pl
+++ Datasets/CKAN/ckan_to_rdf.pl
@@ -13,82 +13,60 @@ Automated CKAN to RDF conversion.
 @version 2014/01
 */
 
-:- use_module(ckan(ckan)).
+:- use_module(ckan(ckan_api)).
+:- use_module(generics(list_script)).
 :- use_module(library(debug)).
-:- use_module(library(lists)).
 
 
-
-:- meta_predicate(goal_list(3,+,+,-)).
-:- meta_predicate(goal_list(3,+,+,+,-)).
-
-goal_list(Goal, Msg, Ins, Outs):-
-  length(Ins, L),
-  goal_list(Goal, Msg, 1-L, Ins, Outs).
-
-goal_list(_, Msg, L-L, [], []):- !.
-goal_list(Goal1, I1-L, [In|Ins], Outs):-
-  Goal1 =.. [Pred|Args1],
-  append(Args1, [In,Out], Args2),
-  Goal2 =.. [Pred|Args2],
-  call(Goal2), !,
-  debug(ckan, '~a ~d/~d', [Msg,I1,L]),
-  I2 is I1 + 1,
-  goal_list(Goal1, Msg, I2-L, Ins, Outs).
-goal_list(Goal, I1-L, [X|Ins], [X|Outs]):-
-  I2 is I1 + 1,
-  goal_list(Goal, I2-L, Ins, Outs).
 
 %! ckan_to_rdf(+Options:list(nvpair)) is det.
 % @see Options are passed to the predicates in module [ckan].
 
-ckan_to_rdf(O1):-
+ckan_to_rdf(O_RDF):-
+  % Conversion to RDF requires presence of graph option.
+  % Conversion to PL requires absence of graph option.
+  select_option(graph(_), O_RDF, O_PL),
   format(current_output, 'Begin CKAN-to-RDF conversion.\n', []),
-
+  
   % Make sure the CKAN site is online.
-  site_read(O1), !,
+  site_read(O_PL), !,
 
   % Groups
-  group_list(O1, _, _, _, _, Groups),
-  goal_list(group_show(O1, _), 'Groups', Groups, GroupsRemaining),
+  group_list(O_PL, _, _, _, _, Groups),
+  list_script(group_show(O_RDF), 'Groups', Groups, GroupsRemaining),
   debug(ckan, 'Remaining groups: ~w', [GroupsRemaining]),
 
   % Licenses.
-  license_list(O1, _),
+  license_list(O_RDF, _),
 
   % Organizations.
-  organization_list(O1, _, _, _, _, Organizations),
-  goal_list(
-    organization_show(O1, _),
+  organization_list(O_PL, _, _, _, _, Organizations),
+  list_script(
+    organization_show(O_RDF),
     'Organizations',
     Organizations,
-    OrganizationsRemaining
+    RemainingOrganizations
   ),
   debug(ckan, 'Remaining organizations: ~w', [RemainingOrganizations]),
 
   % Packages.
-  package_list(O1, _, _, Packages),
-  goal_list(package_show(O1, _), 'Packages', Packages, RemainingPackages),
+  package_list(O_PL, _, _, Packages),
+  list_script(package_show(O_RDF), 'Packages', Packages, RemainingPackages),
   debug(ckan, 'Remaining packages: ~w', [RemainingPackages]),
 
   % Revisions.
-  revision_list(O1, Revisions),
-  goal_list(
-    revision_show(O1, _),
-    'Revisions',
-    Revisions,
-    RemaniningRevisions
-  ),
+  revision_list(O_PL, Revisions),
+  list_script(revision_show(O_RDF), 'Revisions', Revisions, RemainingRevisions),
   debug(ckan, 'Remaining revisions: ~w', [RemainingRevisions]),
 
   % Tags.
-  tag_list(O1, _, _, _, Tags),
-  goal_list(tag_show(O1, _), 'Tags', Tags, RemainingTags),
+  tag_list(O_PL, _, _, _, Tags),
+  list_script(tag_show(O_RDF), 'Tags', Tags, RemainingTags),
   debug(ckan, 'Remaining tags: ~w', [RemainingTags]),
 
   % Users.
-  user_list(O1, _, _, Users),
-  goal_list(user_show(O1, _), 'Users', Users, RemainingUsers),
+  user_list(O_PL, _, _, Users),
+  list_script(user_show(O_RDF, _), 'Users', Users, RemainingUsers),
   debug(ckan, 'Remaining users: ~w', [RemainingUsers]),
 
   format(current_output, 'End CKAN-to-RDF conversion.\n', []).
