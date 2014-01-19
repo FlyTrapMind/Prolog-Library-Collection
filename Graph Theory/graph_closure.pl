c74acbed1bb0f81f619010ff99c1544aea6201c1
Wouter Beek <me@wouterbeek.com>
2014-01-19 23:32:44 +0100

    FIX: graph closure was not calculated correctly.

--- Graph Theory/graph_closure.pl
+++ Graph Theory/graph_closure.pl
@@ -2,7 +2,7 @@
   graph_closure,
   [
     graph_closure/3 % +Elements:ordset
-                    % :NeighborPred
+                    % :NeighborPredicate
                     % -ElementsUnderClosure:ordset
   ]
 ).
@@ -13,26 +13,30 @@
 @version 2013/12
 */
 
-:- use_module(generics(meta_ext)).
 :- use_module(library(lists)).
 :- use_module(library(ordsets)).
 
 :- meta_predicate(graph_closure(+,2,-)).
-:- meta_predicate('_graph_closure'(+,2,+,-)).
+:- meta_predicate(graph_closure(+,2,+,-)).
 
 
 
-graph_closure(Set, NeighborPred, Sol):-
-  '_graph_closure'(Set, NeighborPred, [], Sol).
+%! graph_closure(+Set:ordset, :NeighborPredicate, -ClosedSet:ordset) is det.
+% @arg Set
+% @arg NeighborPredicate Must return the neighbors in ordered sets.
+% @arg ClosedSet
 
-'_graph_closure'([], _NeighborPred, Sol, Sol):-
-  !.
-'_graph_closure'([H|T], NeighborPred, Vs, Sol):-
-  memberchk(H, Vs), !,
-  '_graph_closure'(T, NeighborPred, Vs, Sol).
-'_graph_closure'([H1|T1], NeighborPred, Vs1, Sol):-
-  call(NeighborPred, H1, T2),
-  append(T1, T2, T),
-  ord_union(Vs1, T2, Vs2),
-  '_graph_closure'(T, NeighborPred, Vs2, Sol).
+graph_closure([], _, []):- !.
+graph_closure([H|T], NeighborPred, Sol):-
+  graph_closure([H|T], NeighborPred, [H], Sol).
 
+graph_closure([], _, Sol, Sol):- !.
+graph_closure([H1|T1], NeighborPred, Vs1, Sol):-
+  call(NeighborPred, H1, Neighbors),
+  exclude(in_ordset(Vs1), Neighbors, UnvisitedNeighbors),
+  append(T1, UnvisitedNeighbors, T2),
+  ord_union(Vs1, Neighbors, Vs2),
+  graph_closure(T2, NeighborPred, Vs2, Sol).
+
+in_ordset(Set, Element):-
+  memberchk(Element, Set).
