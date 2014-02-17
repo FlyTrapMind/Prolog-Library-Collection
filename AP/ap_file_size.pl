148628e5edf8efe7b8c245f4a252b0289465edaa
Wouter Beek <me@wouterbeek.com>
2014-02-17 08:00:27 +0100

    * [AP_FILE_SIZE,NEW] Added file size filter, coming from LODObs.

--- AP/ap_file_size.pl
+++ AP/ap_file_size.pl
@@ -1,25 +1,31 @@
 :- module(
   ap_file_size,
   [
-    file_size/3 % +FromDirectory:atom
-                % +ToDirectory:atom
-                % +AP_Stage:iri
+    file_size/3, % +FromDirectory:atom
+                 % +ToDirectory:atom
+                 % +AP_Stage:iri
+    file_size_filter/4 % +FromDirectory:atom
+                       % +ToDirectory:atom
+                       % +AP_Stage:iri
+                       % +MAX_Size_MB:between(0.0,inf)
   ]
 ).
 
 /** <module> AP file size
 
-File size identification for the AP architecture.
+File size identification and filtering for the AP architecture.
 
 @author Wouter Beek
 @version 2014/02
 */
 
 :- use_module(ap(ap_db)).
+:- use_module(library(apply)).
 :- use_module(library(error)).
 :- use_module(library(lists)).
 :- use_module(os(dir_ext)).
 :- use_module(os(file_mime)).
+:- use_module(rdf(rdf_build)).
 :- use_module(rdf(rdf_datatype)).
 
 
@@ -38,4 +44,18 @@ file_size(FromDir, ToDir, AP_Stage):-
     )
   ),
   link_directory_contents(FromDir, ToDir).
+
+
+file_size_filter(FromDir, ToDir, AP_Stage, MAX_Size_MB):-
+  rdf_assert_individual(AP_Stage, ap:'Filter', ap),
+  directory_files([], FromDir, FromFiles),
+  MAX_Size is MAX_Size_MB * 1024 * 1024,
+  maplist(file_size_is_smaller_than(MAX_Size), FromFiles),
+  link_directory_contents(FromDir, ToDir).
+
+file_size_is_smaller_than(MAX_Size, File):-
+  size_file(File, Size),
+  Size =< MAX_Size, !.
+file_size_is_smaller_than(_, File):-
+  permission_error(open, 'BIG-file', File).
 
