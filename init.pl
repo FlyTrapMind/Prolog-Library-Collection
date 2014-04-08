9b1f2bf6749d3d8fa1df1f8c57d5dcafc6402bb2
Wouter Beek <me@wouterbeek.com>
2014-04-08 14:45:11 +0200

    * [DEP,SC] Simplified initialization. * [DEP,PL_CLAS,NEW] Reworte the way in which command-line arguments are handled, using an existing Prolog library.

--- init.pl
+++ init.pl
@@ -5,18 +5,14 @@
 :- use_remote_module(pl(pl_clas)).
 :- use_remote_module(pl(pl_version)).
 
-:- ensure_remote_loaded(plc(pl_debug_option)).
-
 :- initialization(init_pgc).
-
-
 
 init_pgc:-
   % Check SWI-Prolog version.
   check_pl_version,
 
   % Set data subdirectory.
-  process_options(_),
+  process_options,
 
   % Start logging.
   start_log.
--- programming/prolog/pl_clas.pl
+++ programming/prolog/pl_clas.pl
@@ -1,7 +1,7 @@
 :- module(
   pl_clas,
   [
-    process_options/1, % -RemainingOptions:list(nvpair)
+    process_options/0,
     read_options/1 % -Options:list(nvpair)
   ]
 ).
@@ -21,11 +21,13 @@ Support for command line arguments given at Prolog startup.
 :- use_module(library(filesex)).
 :- use_module(library(lists)).
 :- use_module(library(option)).
+:- use_module(library(optparse)).
 :- use_remote_module(os(dir_infra)).
 
 :- multifile(prolog:message//1).
 
-%! cmd_option(?Short:atom, ?Long:atom, ?Type:atom, ?Comment:atom) is nondet.
+
+%! option_specification(?OptionSpecification:list(compound)) is nondet.
 % Command line options registration.
 %
 % @arg Short The short, i.e. one-character, name of the command-line option.
@@ -34,57 +36,94 @@ Support for command line arguments given at Prolog startup.
 %      These types are registered in module [typecheck].
 % @arg Comment A human-readable description of the command-line option.
 
-:- multifile(user:cmd_option/4).
-
-
-%! process_cmd_option(+Options:nvpair) is semidet.
-% Processes a single option.
-
-:- multifile(user:process_cmd_option/1).
-
-
-%! process_cmd_options(+Options1:list(nvpair)) is det.
-% Runs on all options at once.
-%
-% This can be used to check whether some option does _not_ occur.
-
-:- multifile(user:process_cmd_options/1).
+:- multifile(user:option_specification/4).
 
 
 
 % Option: data.
 
-user:cmd_option(_, data, atom, 'Directory where data is read and written.').
+user:option_specification([
+  default(DefaultDataDir),
+  help('The directory where data is stored.'),
+  longflags([data]),
+  opt(data),
+  shortflags([d]),
+  type(boolean)
+]):-
+  absolute_file_name(project(.), ProjectDir, [file_type(directory)]),
+  directory_file_path(ProjectDir, data, DefaultDataDir).
 
-user:process_cmd_option(data(Dir1)):-
+cmd_data_directory(Dir1):-
   absolute_file_name(Dir1, Dir2, [access(write),file_type(directory)]), !,
   set_data_directory(Dir2).
-user:process_cmd_option(data(Dir)):-
+cmd_data_directory(Dir):-
   print_message(warning, incorrect_path(Dir)).
+
+cmd_data_option(O1):-
+  option(data(Dir), O1), !,
+  cmd_data_directory(Dir).
+% Use the default data directory.
+cmd_data_option(O1):-
+  set_data_directory.
 
 prolog:message(incorrect_path(Dir)) -->
   ['The given value could not be resolved to a directory: ',Dir,'.~n'].
 
 
+% Option:debug.
+
+user:option_specification([
+  default(false),
+  help('Run in debug mode. This shows debug messages and loads debug tools.'),
+  longflags([deb,debug]),
+  opt(debug),
+  shortflags([]),
+  type(boolean)
+]).
+
+cmd_debug_option(O1):-
+  option(debug(Debug), O1), !,
+  assert(user:debug_mode),
+  ensure_remote_loaded(pl(pl_debug)).
+cmd_debug_option(_).
+
+
+
 % Option: help.
 
-user:cmd_option(h, help, boolean,
-    'Gives an overview of supported command-line options.').
+user:option_specification([
+  default(false),
+  help('Gives an overview of supported command-line options.'),
+  longflags([help]),
+  opt(help),
+  shortflags([h]),
+  type(boolean)
+]).
 
-user:process_cmd_option(help(true)):-
-  forall(
-    user:cmd_option(Short, Long, _, Comment),
-    describe_option(Short, Long, Comment)
-  ),
+cmd_help(O1):-
+  option(help, O1), !,
+  opt_help(O1, Help),
+  print_message(information, help(Help)),
   halt.
-user:process_cmd_option(help(false)).
+cmd_help(_).
+
+prolog:message(help(Help)) -->
+  [Help].
 
 
 % Option: version.
 
-user:cmd_option(v, version, boolean, 'Display version information.').
+user:option_specification([
+  default(false),
+  help('Display version information.'),
+  longflags([version]),
+  opt(version),
+  shortflags([v]),
+  type(boolean)
+]).
 
-user:process_cmd_option(version(true)):-
+cmd_version(O1):-
+  option(version, O1), !,
   format(
     user_output,
     '  PraSem: Pragmatic Semantics for the Web of Data\n',
@@ -101,164 +140,39 @@ user:process_cmd_option(version(true)):-
     format(user_output, '    * ~w: ~w\n', [Name,Description])
   ),
   halt.
-user:process_cmd_option(version(false)).
+cmd_version(_).
 
 
 
-%! describe_option(?Short:atom, +Long:atom, +Comment:atom) is det.
-% Writes a description for the given command-line option.
-
-describe_option(Short, Long, Comment):-
-  var(Short), !,
-  format(user_output, '        --~w~t~20|~w~n', [Long,Comment]).
-describe_option(Short, Long, Comment):-
-  format(user_output, '    -~w, --~w~t~20|~w~n', [Short,Long,Comment]).
+option_specifications(OptionSpecs):-
+  findall(
+    OptionSpec
+    user:option_specification(OptionSpec),
+    OptionSpecs
+  ).
 
 
-%! long_option(+Name:atom, +Value:atom, -Option:compound) is det.
-% Constructs a long option compound term of the form `NAME=VALUE`,
-% where `VALUE` can be of any type.
+%! process_options is det.
+% Reads the command-line arguments and executes those
+% that are common among the PGC-using projects,
 
-long_option(Long, Value1, Option) :-
-  user:cmd_option(_, Long, Type, _),
-  atom_to_value(Value1, Type, Value2),
-  Option =.. [Long,Value2].
-
-
-%! long_option(+Name:atom, -Option:compound) is det.
-% Constructs a long option compound term of the form `NAME` or `no-NAME`,
-% representing a boolean value of either `true` or `false` respectively.
-%
-% This *only* works for options of type boolean.
-
-% Option with boolean value `false`.
-long_option(Long1, Option):-
-  atom_concat('no-', Long2, Long1),
-  user:cmd_option(_, Long2, boolean, _), !,
-  Option =.. [Long2,false].
-% Option with boolean value `true`.
-long_option(Long, Option):-
-  user:cmd_option(_, Long, boolean, _),
-  Option =.. [Long,true].
-
-
-%! parse_options(
-%!   +CommandLineArguments:list,
-%!   -ParsedOptions:list,
-%!   -Rest:list
-%! ) is det.
-
-parse_options([], [], []):- !.
-% A singular `--` that does not prefix a long option
-% separates the parsed options from the rest.
-parse_options([--|Rest], [], Rest):- !.
-% A long option.
-parse_options([H|T], [Option|Options], Rest):-
-  sub_atom(H, 0, _, _, --), !,
-  % A long name-value option,
-  % or a name option.
-  (
-    sub_atom(H, B, _, A, =)
-  ->
-    B2 is B - 2,
-    sub_atom(H, 2, B2, _, Long),
-    sub_atom(H, _, A,  0, Value),
-    long_option(Long, Value, Option)
-  ;
-    sub_atom(H, 2, _, 0, Long),
-    long_option(Long, Option)
-  ),
-  parse_options(T, Options, Rest).
-% A short option.
-parse_options([H|T], [Option|Options], Rest):-
-  atom_chars(H, [-|Short]), !,
-  short_option(Short, Option),
-  parse_options(T, Options, Rest).
-% Unrecognized options are returned in `Rest`.
-parse_options(Rest, [], Rest).
-
-
-%! process_options(
-%!   -ExecutedOptions:list(nvpair),
-%!   -RemainingOptions:list(nvpair)
-%! ) is det.
-% Reads the command-line arguments and executes those that are common
-% among the PGC-using projects,
-% e.g. setting the directory for reading/writing data files
-% using `--data=DIR`.
-%
-% Only the options that are not processed in a generic way,
-% i.e. those that are application-specific, are returned.
-
-process_options(O4):-
-gtrace,
+process_options:-
   read_options(O1), !,
-
+  
   % First set the data directory,
   % since other command-line arguments may depend on it being set,
   % e.g. `project=NAME`.
-  (
-    select_option(data(Dir), O1, O2)
-  ->
-    user:process_cmd_option(data(Dir))
-  ;
-    % Use the default data directory.
-    set_data_directory,
-    O2 = O1
-  ),
+  cmd_data_option(O1),
   
   % Then set the debug mode and load the debug tools.
-  (
-    select_option(debug(Debug), O2, O3)
-  ->
-    user:process_cmd_option(debug(Debug))
-  ;
-    O3 = O2
-  ),
+  cmd_debug_option(O1),
   
-  % Process command-line arguments that change the set of options,
-  % e.g. `help`.
-  process_all_options(O3),
+  cmd_help(O1),
+  
+  cmd_version(O1).
 
-  exclude(user:process_cmd_option, O3, O4).
-process_options(_):-
-  print_message(warning, clas_parse_failed),
-  halt.
-
-prolog:message(clas_parse_failed) -->
-  ['Could not parse command-line arguments.'].
-
-
-%! process_all_options(+Options1:list(nvpair)) is det.
-
-process_all_options(O1):-
-  user:process_cmd_options(O1),
-  % Enumerate by fail.
-  fail, !.
-process_all_options(_).
-
-
-%! read_options(-Options:list(nvpair)) is det.
-% Returns the command-line arguments, either long (`--`) or short (`-`)
-% notation, in the form of name-value pairs / options.
 
 read_options(Options):-
-  current_prolog_flag(argv, Argv),
-  parse_options(Argv, Options, _).
-
-
-%! short_option(+Short:atom, -Option:compound) is det.
-% Constructs a short option.
-%
-% Short options are always abbreviations of long options
-% and always have the type boolean.
-%
-% The value of a short option, when used, is `true`,
-% and `false` otherwise, i.e. when not used.
-
-short_option(Short1, Option):-
-  user:cmd_option(Short2, Long, boolean, _),
-  % Do not match against options with no short name.
-  Short1 == Short2,
-  Option =.. [Long,true].
+  options_specifications(OptionSpecs),
+  opt_arguments(OptionSpecs, Options, _).
 
