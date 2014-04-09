a5d19f2bdcfd61727563abe8edb5df012d9802c2
Wouter Beek <me@wouterbeek.com>
2014-04-10 01:02:43 +0200

    deb

--- use_remote_module.pl
+++ use_remote_module.pl
@@ -306,7 +306,7 @@ use_remote_module(ModuleSpec):-
   use_remote_module(DefaultRepository, ModuleSpec).
 
 use_remote_module(RepositoryId, CallingModule:CalledModuleSpec):-
-(flag(numer_of_downloaded_files, X, X), X >= 34 -> gtrace ; true), %DEB
+(flag(number_of_downloaded_files, X, X), X >= 34 -> gtrace ; true), %DEB
   flag(level_of_nesting, M, M + 1),
   fetch_remote_file(RepositoryId, CalledModuleSpec, LocalFile),
   CallingModule:use_module(LocalFile),
