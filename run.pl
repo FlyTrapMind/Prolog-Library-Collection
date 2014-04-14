8b0102064dd4e953ca906a9c08549c6e39b201d5
Wouter Beek <me@wouterbeek.com>
2014-04-14 12:55:57 +0200

    sas

--- run.pl
+++ run.pl
@@ -1,18 +1,16 @@
 % Run file for PLC.
 % This is used to run the PLC independent of any other projects/submodules.
 
-:- use_module(library(filesex)).
-
 :- initialization(run_plc).
 
 run_plc:-
   % Assert project file search path.
   absolute_file_name('.', ProjectDir, [access(read),file_type(directory)]),
   assert(user:file_search_path(project, ProjectDir)),
-  
+
   ensure_loaded(prolog_repository),
   prolog_repository(local, ProjectDir),
-  
+
   % Load PLC.
   ensure_loaded(load).
 
