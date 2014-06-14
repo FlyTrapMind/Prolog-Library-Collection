b7a672765dc90033b4d64788d0b542109bef15e7
Wouter Beek <me@wouterbeek.com>
2014-06-14 04:13:01 +0200

    * [LOD_QUERY,SC] Removed module, duplication of features in project lodCache. * [SPARQL_CACHE,SC] Vastly simplified code. * [SPARQL_FIND,SC] Vastly simplified code.

--- lod/sparql/sparql_cache.pl
+++ lod/sparql/sparql_cache.pl
@@ -5,40 +5,36 @@
                     % +Graph:atom
     sparql_cache/3 % +Resource:or([bnode,iri,literal])
                    % -Resources:ordset(or([bnode,iri,literal]))
-                   % -Propositions:ordset(list)
+                   % -Propositions:ordset(list(or([bnode,iri,literal])))
   ]
 ).
 
 /** <module> SPARQL Cache
 
-Locally caches triples that are relevant for specific resources.
+Locally caches triples that are relevant for a given resource.
 
 @author Wouter Beek
 @version 2014/01-2014/02, 2014/06
 */
 
-:- use_module(library(apply)).
-:- use_module(library(debug)).
+:- use_module(library(lists)).
 :- use_module(library(ordsets)).
 :- use_module(library(semweb/rdf_db)).
-:- use_module(library(uri)).
 
-:- use_module(generics(row_ext)).
-:- use_module(sparql(sparql_api)).
-:- use_module(sparql(sparql_db)).
-
-:- debug(sparql_cache).
+:- use_module(plSparql(sparql_api)).
+:- use_module(plSparql(sparql_db)).
 
 
 
-%! sparql_cache(+Resource:iri, +Graph:atom) is det.
+%! sparql_cache(+Resource:or([bnode,iri,literal]), +Graph:atom) is det.
+% Wrapper around sparql_cache/3, where the set of resources does not matter.
 
 sparql_cache(Resource, Graph):-
   sparql_cache(Resource, _, Propositions),
-  maplist(assert_proposition(Graph), Propositions).
-
-assert_proposition(Graph, [S,P,O]):-
-  rdf_assert(S, P, O, Graph).
+  forall(
+    member([S,P,O], Propositions),
+    rdf_assert(S, P, O, Graph)
+  ).
 
 
 %! sparql_cache(
@@ -48,79 +44,19 @@ assert_proposition(Graph, [S,P,O]):-
 %! ) is det.
 % Returns the depth-one resources and propositions for the given resource.
 %
-% The given resource is assumed to be located on a URL which has
-%  a registered SPARQL endpoint.
+% This only gives non-trivial results if the resource term is a URL
+% which denotes a registered SPARQL endpoint.
 
 % Blank node.
 sparql_cache(Resource, [], []):-
   rdf_is_bnode(Resource), !.
-
 % Literal.
 sparql_cache(Resource, [], []):-
   rdf_is_literal(Resource), !.
-
-% Skip IRI based on parsing (part of) the IRI itself.
-sparql_cache(Resource, [], []):-
-  uri_components(
-    Resource,
-    uri_components(Scheme,Domain,Path,_Fragment,_Search)
-  ),
-  skip_iri(Scheme, Domain, Path).
-
-% IRI with registered SPARQL endpoint.
+% IRI with a registered SPARQL endpoint.
 sparql_cache(Resource, Resources, Propositions):-
-  % Find an endpoint for the given resource.
-  uri_components(Resource, uri_components(Scheme,Authority,_,_,_)),
-  uri_components(Prefix, uri_components(Scheme,Authority,_,_,_)),
-  sparql_prefix(Endpoint, Prefix),
-  
-  % Query.
-  sparql_select(Endpoint, _, [], true, [p,o],
-      [rdf(iri(Resource), var(p), var(o))], inf, _, _, Rows, []),
-
-  % Conversion
-  rows_to_propositions([Resource], Rows, Propositions),
+  sparql_endpoint_by_resource(Resource, Endpoint),
+  sparql_select(Endpoint, [], [p,o], [
+      rdf(iri(Resource),var(p),var(o))], Propositions, [distinct(true)]),
   ord_union(Propositions, Resources).
-
-%! rows_to_propositions(
-%!   +Prefix:list([bnode,literal,iri]),
-%!   +Rows:list(compound),
-%!   -Propositions:ordset(list)
-%! ) is det.
-% Returns the ordered set of propositions that occur in
-%  the given SPARQL result set rows.
-%
-% @arg Prefix This contains the stable prefix list of each proposition.
-%      This is usually the singleton list of the subject term.
-% @arg Rows
-% @arg Propositions An ordered set of lists of length 3 (s-p-o).
-
-rows_to_propositions(Prefix, Rows, Props):-
-  rows_to_propositions(Prefix, Rows, [], Props).
-
-rows_to_propositions(_, [], Sol, Sol):- !.
-rows_to_propositions(Prefix, [H1|T], L1, Sol):-
-  row_to_proposition(Prefix, H1, H2),
-  ord_add_element(L1, H2, L2),
-  rows_to_propositions(Prefix, T, L2, Sol).
-
-row_to_proposition(Prefix, Row, L):-
-  Row =.. [row|Suffix],
-  append(Prefix,  Suffix, L).
-
-
-skip_iri(_, Domain, _):-
-  atomic_list_concat(DomainComponents, '.', Domain),
-  member(DomainComponent, DomainComponents),
-  member(DomainComponent, [wikidata,wikipedia]), !.
-
-% Skip IRI based on file extension.
-skip_iri(Scheme, Domain, Path):-
-  uri_components(
-    ResourceWithoutFragmentOrSearch,
-    uri_components(Scheme,Domain,Path,_NoFragment,_NoSearch)
-  ),
-  file_type(Type, ResourceWithoutFragmentOrSearch),
-  nonvar(Type),
-  member(Type, [html,image,pdf]), !.
 
