f3b453b386aff2938219b36c6bf7eabc35a3db47
Wouter Beek <me@wouterbeek.com>
2014-06-27 05:45:17 +0200

    * [FLAG_EXT,NEW] Added state flag.

--- generics/flag_ext.pl
+++ generics/flag_ext.pl
@@ -6,9 +6,16 @@
                       % +N:positive_integer
                       % :Goal
       reset_thread_flag/1, % +Flag:atom
-      thread_flag/3 % +Flag:atom
-                    % -Old
-                    % +New
+      thread_flag/3, % +Flag:atom
+                     % -Old
+                     % +New
+% State
+      state_init/1, % -State:compound
+      state_read/2, % +State:compound
+                    % -N:nonneg
+      state_tick/1, % +State:compound
+      state_tick/2 % +State:compound
+                   % -N:nonneg
     ]
   ).
 
@@ -54,5 +61,20 @@ thread_flag(Flag, X, Y):-
 
 
 
+% State
 
+state_init(state(0)).
+
+
+state_read(State, N):-
+  arg(1, State, N).
+
+
+state_tick(State):-
+  state_tick(State, _).
+
+state_tick(State, C1):-
+  arg(1, State, C0),
+  C1 is C0 + 1,
+  nb_setarg(1, State, C1).
 
