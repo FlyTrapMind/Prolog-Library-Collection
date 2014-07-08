40e9c189d30ba1943b181b170def7d234bf8c3af
Wouter Beek <me@wouterbeek.com>
2014-07-08 17:17:35 +0200

    * [SC] Removed the thread_flag predicates. They did not work properly, and they use an outdated technology.

--- generics/flag_ext.pl
+++ generics/flag_ext.pl
@@ -1,16 +1,6 @@
 :- module(
   flag_ext,
     [
-      call_every_n/3, % +Flag:atom
-                      % +N:positive_integer
-                      % :Goal
-      reset_thread_flag/1, % +Flag:atom
-      thread_flag/2, % +Flag:atom
-                     % ?Current
-      thread_flag/3, % +Flag:atom
-                     % -Old
-                     % +New
-% State
       state_init/1, % -State:compound
       state_read/2, % +State:compound
                     % -N:nonneg
@@ -24,43 +14,11 @@
 
 Extensions for flags and shared variables.
 
-@auhtor Wouter Beek
-@version 2014/06
+@author Wouter Beek
+@version 2014/06-2014/07
 */
 
-:- meta_predicate(call_every_n(+,+,1)).
 
-
-
-call_every_n(Flag, N, Goal):-
-  thread_flag(Flag, M, M + 1),
-  (
-    M > 0,
-    0 =:= M mod N
-  ->
-    call(Goal, M)
-  ;
-    true
-  ).
-
-
-%! reset_thread_flag(+Flag:atom) is det.
-
-reset_thread_flag(Flag):-
-  thread_self(Thread),
-  flag(Thread-Flag, _, 0).
-
-
-thread_flag(Flag, X):-
-  thread_flag(Flag, X, X).
-
-thread_flag(Flag, X, Y):-
-  thread_self(Thread),
-  flag(Thread-Flag, X, Y).
-
-
-
-% State
 
 state_init(state(0)).
 
