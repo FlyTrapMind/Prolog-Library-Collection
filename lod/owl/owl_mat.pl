346e2198a7684801a0f22aee960fcd9f3fa7694b
Wouter Beek <me@wouterbeek.com>
2014-07-29 14:27:44 +0200

    * [OWL_MAT,NEW] Added owl_materialize/1 wrapper.

--- lod/owl/owl_mat.pl
+++ lod/owl/owl_mat.pl
@@ -1,15 +1,18 @@
 :- module(
   owl_mat,
   [
-    owl_materialize/2 % +FromDir:atom
-                      % +ToDir:atom
+    owl_materialize/1, % +Directory:atom
+    owl_materialize/2 % +FromDirectory:atom
+                      % +ToDirectory:atom
   ]
 ).
 
-/** <module> RDF predicates for automated processes
+/** <module> OWL materialization
+
+OWL materialization support.
 
 @author Wouter Beek
-@version 2013/11-2013/12
+@version 2013/11-2013/12, 2014/07
 */
 
 :- use_module(generics(db_ext)).
@@ -19,11 +22,18 @@
 
 
 
+%! owl_materialize(+Directory:atom) is det.
+
+owl_materialize(Dir):-
+  owl_materialize(Dir, Dir).
+
+%! owl_materialize(+FromDirectory:atom, +ToDirectory:atom) is det.
+
 owl_materialize(FromDir, ToDir):-
   absolute_file_name(
     iotw('iotw-0.0.1-SNAPSHOT'),
-    JAR_File,
+    JarFile,
     [access(read),file_type(jar)]
   ),
-  run_jar(JAR_File, [file(FromDir),file(ToDir)]).
+  run_jar(JarFile, [file(FromDir),file(ToDir)]).
 
