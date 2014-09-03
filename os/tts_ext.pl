616edac6ab33c8b5fc6696ebcaf24cf006fa81a0
Wouter Beek <me@wouterbeek.com>
2014-09-03 12:29:32 +0200

    * [TTS_EXT,SC] Simplified input argument.

--- os/tts_ext.pl
+++ os/tts_ext.pl
@@ -1,7 +1,7 @@
 :- module(
   tts_ext,
   [
-    text_to_speech/1 % +Contents
+    text_to_speech/1 % +Input:or([atom,string])
   ]
 ).
 
@@ -24,32 +24,23 @@ user:module_uses(tts_ext, program(espeak)).
 
 
 
-%! text_to_speech(+Contents) is det.
+%! text_to_speech(+Input:or([atom,string])) is det.
 % Creates a process for a program that can turn text into speech.
 %
 % This is currently implemented for Unix systems with espeak in the PATH.
 %
 % @tbd Add support for Windows.
-% @tbd Test support on Mac OS-X.
+% @tbd Test support for Mac.
 
-text_to_speech(Format-Arguments):- !,
-  format(atom(Atom), Format, Arguments),
-  text_to_speech(Atom).
-text_to_speech(Terms):-
-  is_list(Terms), !,
-  maplist(term_to_atom, Terms, Atoms),
-  atomic_list_concat(Atoms, '\n', Atom),
-  text_to_speech(Atom).
-text_to_speech(Atom):-
-  atom(Atom), !,
-  os_dependent_call(text_to_speech(Atom)).
+text_to_speech(Input):-
+  os_dependent_call(text_to_speech(Input)).
 
 :- if(is_unix).
-text_to_speech_unix(Atom):-
-  process_create(path(espeak), [Atom], [detached(true)]).
+text_to_speech_unix(Input):-
+  process_create(path(espeak), [Input], [detached(true)]).
 :- endif.
 
 :- if(is_windows).
-text_to_speech_windows(_Atom).
+text_to_speech_windows(_).
 :- endif.
 
