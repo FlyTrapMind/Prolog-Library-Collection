cab582626e6ba2e00e2be5cf1901b2ed89f0851a
Wouter Beek <me@wouterbeek.com>
2014-09-08 14:00:54 +0200

    * [REQUEST_EXT,NEW] Added dummy predicates for request_filter/[4,5]. TBD.

--- generics/request_ext.pl
+++ generics/request_ext.pl
@@ -1,6 +1,15 @@
 :- module(
   request_ext,
   [
+    request_filter/4, % +Request:list(nvpair)
+                      % +Method:oneof([delete,get,head,option,post,put])
+                      % +Accept:pair(atom)
+                      % -Location:url
+    request_filter/5, % +Request:list(nvpair)
+                      % +Method:oneof([delete,get,head,option,post,put])
+                      % +Accept:atom
+                      % -Location:url
+                      % -LocationComponents:pair(url,atom)
     request_query_nvpair/3, % +Request:list(nvpair)
                             % +Name:atom
                             % -Value
@@ -19,9 +28,29 @@
 Additional support for requests.
 
 @author Wouter Beek
-@version 2014/08
+@version 2014/08-2014/09
 */
 
+:- use_module(library(http/http_cors)).
+
+
+
+request_filter(Request, Method, Accept, Location):-
+gtrace,
+  cors_enable,
+  writeln(Request),
+  Method = Request,
+  Accept = Request,
+  Location = Request.
+
+
+request_filter(Request, Method, Accept, Location, LocationComponents):-
+  request_filter(Request, Method, Accept, Location),
+  split_location(Request, Location, LocationComponents).
+
+split_location(_Request, Location, Base-Path):-
+  Base = Location,
+  Path = Location.
 
 
 %! request_query_nvpair(+Request:list(nvpair), +Name:atom, -Value) is det.
