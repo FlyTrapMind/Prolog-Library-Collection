90e733fd4c1c71be80be397db0e9eb41bd421e77
Wouter Beek <me@wouterbeek.com>
2014-09-22 09:07:38 +0200

    Fixed: module name <-> file name

--- generics/lambda_meta.pl
+++ generics/lambda_meta.pl
@@ -34,7 +34,7 @@ official policies, either expressed or implied, of Ulrich Neumerkel.
 
 */
 
-:- module(lambda, [
+:- module(lambda_meta, [
 		   (^)/3, (^)/4, (^)/5, (^)/6, (^)/7, (^)/8, (^)/9,
 		   (\)/1, (\)/2, (\)/3, (\)/4, (\)/5, (\)/6, (\)/7,
 		   (+\)/2, (+\)/3, (+\)/4, (+\)/5, (+\)/6, (+\)/7,
