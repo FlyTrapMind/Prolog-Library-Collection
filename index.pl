427098b966a4d31cb283ab5f800a624bf25f6694
Wouter Beek <me@wouterbeek.com>
2014-10-31 21:51:43 +0100

    Relocated very many modules.

--- index.pl
+++ index.pl
@@ -2,16 +2,8 @@
 % File search path `plc` must be set for this.
 
 user:file_search_path(generics,        plc(generics)).
-user:file_search_path(ilp,             plc(ilp)).
-user:file_search_path(lod,             plc(lod)).
-user:file_search_path(owl,             lod(owl)).
-user:file_search_path(skos,            lod(skos)).
-user:file_search_path(sparql,          lod(sparql)).
-user:file_search_path(void,            lod(void)).
 user:file_search_path(math,            plc(math)).
 user:file_search_path(os,              plc(os)).
 user:file_search_path(pl,              plc(prolog)).
-user:file_search_path(standards,       plc(standards)).
-user:file_search_path(datetime,        standards(date_time)).
-user:file_search_path(geo,             standards(geography)).
+user:file_search_path(datetime,        plc(date_time)).
 
