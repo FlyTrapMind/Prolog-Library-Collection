3cf13dad04fbb9cc39e6becf2646cdbe2308f8c3
Wouter Beek <me@wouterbeek.com>
2014-12-19 10:25:27 +0100

    ENHANCED: Simplified calling a JAR from within Prolog.

--- os/java_ext.pl
+++ os/java_ext.pl
@@ -1,12 +1,9 @@
 :- module(
   java_ext,
   [
-    run_jar/2, % +Jar:atom
-               % +Args:list
-    run_jar/4 % +Jar:atom
+    run_jar/3 % +Jar:atom
               % +Args:list
-              % ?Error:stream
-              % ?Output:stream
+              % +Options:list(nvpair)
   ]
 ).
 
@@ -15,11 +12,12 @@
 Extensions for executing Java JARs from within Prolog.
 
 @author Wouter Beek
-@version 2014/01-2014/02, 2014/12
+@version 2014/12
 */
 
-:- use_module(library(apply)).
+:- use_module(library(option)).
 :- use_module(library(process)).
+:- use_module(library(readutil)).
 
 :- use_module(generics(db_ext)).
 :- use_module(os(run_ext)).
@@ -27,31 +25,39 @@ Extensions for executing Java JARs from within Prolog.
 :- db_add_novel(user:prolog_file_type(jar, jar)).
 :- db_add_novel(user:prolog_file_type(log, log)).
 
+:- predicate_options(run_jar/3, 3, [
+     pass_to(process_create/3, 3)
+   ]).
 
 
 
 
-%! run_jar(+Jar:atom, +Args:list(atom)) is det.
-% Runs the given JAR file with the given commandline arguments,
-% but does not use the output or error stream.
 
-run_jar(Jar, Args):-
-  run_jar(Jar, Args, Output, Error),
-  maplist(close, [Output,Error]).
+%! run_jar(+Jar:atom, +Args:list, +Options:list(nvpair)) is det.
+% Runs the given JAR file with the given commandline arguments.
+%
+% Options are passed to process_create/3.
 
-
-
-%! run_jar(+Jar:atom, +Args:list(atom), -Error:stream, -Output:stream) is det.
-% Runs the given JAR file with the given commandline arguments,
-% and uses the given output streams for Java error and Java output.
-
-run_jar(Jar, Args, Error, Output):-
-  process_create(
-    path(java),
-    ['-jar',file(Jar)|Args],
-    [process(Pid),stderr(pipe(Error)),stdout(pipe(Output))]
-  ),
+run_jar(Jar, Args, Options1):-
+  merge_options(Options1, [process(Pid),stderr(pipe(Error))], Options2),
+  process_create(path(java), ['-jar',file(Jar)|Args], Options2),
+  
+  % Communicate that end-of-process has occurred.
   process_wait(Pid, exit(ShellStatus)),
   file_base_name(Jar, JarName),
   format(atom(Program), 'Java/JAR ~a', [JarName]),
-  exit_code_handler(Program, ShellStatus).
+  exit_code_handler(Program, ShellStatus),
+  
+  % Process error stream.
+  read_stream_to_codes(Error, ErrorCodes),
+  print_error(ErrorCodes),
+  close(Error).
+
+print_error([]):- !.
+print_error(ErrorCodes):-
+  string_codes(ErrorString, ErrorCodes),
+  print_message(warning, process_error(ErrorString)).
+
+:- multifile(prolog:message//1).
+prolog:message(rdf_compress_error(ErrorString)) --> [ErrorString].
+
