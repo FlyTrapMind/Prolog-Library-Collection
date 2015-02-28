7d373f8dcfb68ae8884ceb29ac03028ba0b6ddd6
Wouter Beek <me@wouterbeek.com>
2015-02-28 22:00:04 +0100

    ENHANCED: Count flag ad debug messages added to replace-in-file.

--- dcg/replace_in_file.pl
+++ dcg/replace_in_file.pl
@@ -2,8 +2,8 @@
   replace_in_file,
   [
     replace_in_file/4, % +OldFile:atom
-                       % :FromDCG
-                       % :ToDCG
+                       % :FromDcg
+                       % :ToDcg
                        % ?NewFile
     trim_spaces/2 % +OldFile:atom
                   % ?NewFile:atom
@@ -15,13 +15,15 @@
 Make arbitrary consecutive replacements in text files.
 
 @author Wouter Beek
-@version 2013/09, 2014/10-2014/12
+@version 2013/09, 2014/10-2014/12, 2015/02
 */
 
+:- use_module(library(apply)).
 :- use_module(library(debug)).
 
 :- use_module(plc(dcg/dcg_abnf)).
 :- use_module(plc(dcg/dcg_ascii)).
+:- use_module(plc(dcg/dcg_content)).
 :- use_module(plc(dcg/dcg_generics)).
 :- use_module(plc(dcg/dcg_replace)).
 :- use_module(plc(generics/atom_ext)). % Meta-option.
@@ -36,9 +38,9 @@ Make arbitrary consecutive replacements in text files.
 
 
 
-%! replace_in_file(+OldFile:atom, :FromDCG, :ToDCG, ?NewFile:atom) is det.
+%! replace_in_file(+OldFile:atom, :FromDcg, :ToDcg, ?NewFile:atom) is det.
 
-replace_in_file(F1, FromDCG, ToDCG, F2):-
+replace_in_file(F1, FromDcg, ToDcg, F2):-
   % Type checks.
   is_absolute_file_name(F1),
   xor(
@@ -48,37 +50,34 @@ replace_in_file(F1, FromDCG, ToDCG, F2):-
   ),
   setup_call_cleanup(
     open(F2, write, Out, [encoding(utf8),type(text)]),
-    phrase_from_file(replace_in_file(Out, FromDCG, ToDCG), F1),
+    phrase_from_file(replace_in_file(Out, FromDcg, ToDcg), F1),
     close(Out)
   ).
 
 
 
-%! replace_in_file(+Output:stream, :FromDCG, :ToDCG)// is det.
+%! replace_in_file(+Output:stream, :FromDcg, :ToDcg)// is det.
 
-replace_in_file(Out, FromDCG, ToDCG) -->
-  dcg_until(end_of_line, L1, [convert(codes_atom),end_mode(inclusive)]), !,
+replace_in_file(Out, FromDcg, ToDcg) -->
+  '...'(Line1),
+  end_of_replace_line, !,
   {
     % Replace line
-    phrase(dcg_replace(FromDCG, ToDCG), L1, L2),
+    once(phrase(dcg_replace(FromDcg, ToDcg), Line1, Line2)),
 
     % DEB
-    (
-      L1 \= L2
-    ->
-      atom_codes(A1, L1),
-      atom_codes(A2, L2),
-      debug(replace_in_file, '~w\n~w\n', [A1,A2])
-    ;
-      true
+    (   Line1 \= Line2
+    ->  maplist(atom_codes, [Atom1,Atom2], [Line1,Line2]),
+        debug(replace_in_file, 'FROM:\t~a\nTO:\t~a\n', [Atom1,Atom2])
+    ;   true
     ),
 
     % Write line
-    put_codes(Out, L2),
+    put_codes(Out, Line2),
     flush_output(Out)
   },
-  replace_in_file(Out, FromDCG, ToDCG).
-replace_in_file(_Out, _DCG_From, _DCG_To) --> [].
+  replace_in_file(Out, FromDcg, ToDcg).
+replace_in_file(_, _, _) --> [].
 
 
 
@@ -87,3 +86,15 @@ replace_in_file(_Out, _DCG_From, _DCG_To) --> [].
 trim_spaces(F1, F2):-
   replace_in_file(F1, '+'(space, []), space, F2).
 
+
+
+
+
+% HELPERS %
+
+end_of_replace_line -->
+  end_of_line,
+  {
+    flag(replace_lines, N, N + 1),
+    debug(replace_in_file, 'REPLACE-IN-FILE ~D', [N])
+  }.
