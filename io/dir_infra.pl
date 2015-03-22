81db6dcefa4ded0a3b1052903d52c951ecf97366
Wouter Beek <me@wouterbeek.com>
2015-03-22 09:12:26 +0100

    1. CLEAN: Removed legacy directory infrastructure predicates. 2. ADDED: create_project_subdirectory/[1,2].

--- io/dir_infra.pl
+++ io/dir_infra.pl
@@ -1,98 +1,45 @@
 :- module(
   dir_infra,
   [
-    create_personal_subdirectory/2, % +Subdirs:list(atom)
-                                    % -AbsoluteDir:atom
-    home_directory/1, % -HomeDir:atom
-    output_directory/1, % -OutputDir:atom
-    trashcan/1 % -TrashDir:atom
+    create_project_subdirectory/1, % +Subdir:atom
+    create_project_subdirectory/2 % +Subdir:atom
+                                  % -Path:atom
   ]
 ).
 
 /** <module> Directory infrastructure
 
-Predicates for creating an maintaining a specific infrastructure of
-directories that are used for certain purposes, e.g. output, data, trash.
-
 @author Wouter Beek
-@version 2013/06-2013/07, 2013/09, 2013/11-2014/02, 2014/04, 2014/10-2014/11
+@version 2015/03
 */
 
+:- use_module(library(filesex)).
+
 :- use_module(plc(io/dir_ext)).
-:- use_module(plc(io/file_ext)).
 
 
 
 
 
-%! create_personal_subdirectory(
-%!   +Subdirs:list(atom),
-%!   -AbsoluteDir:atom
-%! ) is det.
-% Asserts a project-specific directory that is a direct subdirectory of the
-% current user's home.
-%
-% This can be used to write files to. Something that is not guaranteed for
-% the location where the code is run from.
-%
-% This requires that the project name has been set using project/2.
+%! create_project_subdirectory(+Subdir:atom) is det.
 
-create_personal_subdirectory(Subdirs, AbsDir):-
-  % Make sure the personal directory is there.
-  personal_directory_init,
-  create_directory(personal, Subdirs, AbsDir).
+create_project_subdirectory(Subdir):-
+  create_project_subdirectory(Subdir, _).
 
 
 
-%! home_directory(-HomeDir:atom) is det.
+%! create_project_subdirectory(+Subdir:atom, -Path:atom) is det.
 
-home_directory(HomeDir):-
-  expand_file_name('~/', HomeDir).
-
-
-
-%! output_directory(-OutputDir:atom) is det.
-
-output_directory(OutputDir):-
-  create_directory(data, ['Output'], OutputDir).
-
-
-
-%! trashcan(-TrashDir:atom) is det.
-
-trashcan(TrashDir):-
-  trashcan_init,
-  create_directory(personal, ['Trash'], TrashDir).
-
-
-
-% HELPERS
-
-home_init:-
-  file_search_path(home, _), !.
-home_init:-
-  expand_file_name('~', [HomeDir]),
-  assert(user:file_search_path(home, HomeDir)).
-
-
-
-personal_directory_init:-
-  file_search_path(personal, _), !.
-personal_directory_init:-
-  % Make sure the home directory is there.
-  home_init,
-
-  % @tbd Multiple projects can be loaded at the same time.
-  user:project(Project, _, _),
-
-  hidden_file_name(Project, Hidden),
-  create_directory(home, [Hidden], _),
-  assert(user:file_search_path(personal, home(Hidden))).
-
-
-
-trashcan_init:-
-  file_search_path(trash, _), !.
-trashcan_init:-
-  create_personal_subdirectory('Trash', Dir),
-  assert(user:file_search_path(trash, Dir)).
+create_project_subdirectory(Subdir, Path):-
+  project(Name, _),
+  Spec =.. [Name,Subdir],
+  (   absolute_file_name(
+        Spec,
+        Path,
+        [access(read),file_type(directory),file_errors(fail)]
+      )
+  ->  true
+  ;   absolute_file_name(Name, Dir, [access(write),file_type(directory)]),
+      directory_file_path(Dir, rdf, Path),
+      make_directory_path(Path)
+  ).
