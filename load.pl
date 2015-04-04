ae7f6226dbdabfa620a0ebf6503ed20e02c6189a
Wouter Beek <me@wouterbeek.com>
2015-04-04 12:47:09 +0200

    CLEAN: minor

--- load.pl
+++ load.pl
@@ -1,10 +1,9 @@
 % The load file for the Project Library Collection,
 
-:- use_module(library(ansi_term)).
-
 :- dynamic(user:project/3).
 :- multifile(user:project/3).
 user:project('Prolog-Library-Collection', 'Prolog Library Collection', plc).
+
 
 :- use_module(load_project).
 :- load_project([
