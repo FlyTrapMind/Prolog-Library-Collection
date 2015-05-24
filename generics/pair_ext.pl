b932540d10354bc9f63fcb2fe070c56e36b37a91
Wouter Beek <me@wouterbeek.com>
2015-05-24 23:40:38 +0200

    ADDED: group_pairs_by_key/2 variant using =@=/2 i.o. ==/2.

--- generics/pair_ext.pl
+++ generics/pair_ext.pl
@@ -1,6 +1,8 @@
 :- module(
   pair_ext,
   [
+    'group_pairs_by_key@'/2, % +Pairs:list(pair)
+                             % -GroupedPairs:list(pair)
     inverse_pair/2, % ?Pair:pair
                     % ?Inverse:pair
     json_pair/2, % ?Pair:pair
@@ -95,6 +97,21 @@ error:has_type(pair(Type), X-Y):-
   error:has_type(pair(Type,Type), X-Y).
 error:has_type(pair(Type1,Type2), X-Y):-
   maplist(error:has_type, [Type1,Type2], [X,Y]).
+
+
+
+% ! 'group_pairs_by_key@'(+Pairs:list(pair), -GroupedPairs:list(pair))
+% is det.
+
+'group_pairs_by_key@'([], []).
+'group_pairs_by_key@'([M-N|T0], [M-[N|TN]|T]):-
+  'same_key@'(M, T0, TN, T1),
+  'group_pairs_by_key@'(T1, T).
+
+'same_key@'(M0, [M-N|T0], [N|TN], T):-
+  M0 =@= M, !,
+  'same_key@'(M, T0, TN, T).
+'same_key@'(_, L, [], L).
 
 
 
