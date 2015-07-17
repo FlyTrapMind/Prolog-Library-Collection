74a0583e24ef02a043fab4128b22c286b11f88b6
Wouter Beek <me@wouterbeek.com>
2015-07-17 13:57:33 +0200

    minor

--- pack.pl
+++ pack.pl
@@ -2,7 +2,7 @@ author('Wouter Beek', 'me@wouterbeek.com').
 download('https://github.com/wouterbeek/Prolog-Library-Collection/release/*.zip').
 home('https://github.com/wouterbeek/Prolog-Library-Collection').
 maintainer('Wouter Beek', 'me@wouterbeek.com').
-name(plc).
+name('Prolog-Library-Collection').
 packager('Wouter Beek', 'me@wouterbeek.com').
 title('Prolog-Library-Collection').
 version('0.0.1').
